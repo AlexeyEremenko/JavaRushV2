3446803d2974016cbb3345d293240748da5a3a6e
AlexEremenko <alex.eremenko@gmail.com>
2020-02-19 15:58:41 +0600

    Some message

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        //напишите тут ваш код
+        System.out.println("Над седой равниной моря");
+        System.out.println("Гордо реет жирный пингвин");
+        System.out.println("Он и сам уже не помнит");
+        System.out.println("Как сумел с земли подняться...");
+    }
+}
