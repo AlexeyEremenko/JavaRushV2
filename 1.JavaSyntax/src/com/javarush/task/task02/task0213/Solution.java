e67222aaf4f03df0495e218806fc55c9998c196c
AlexEremenko <alex.eremenko@gmail.com>
2020-02-20 17:27:55 +0600

    Commit message

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0213/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task02.task0213;
+
+/* 
+Питомцам нужны люди
+*/
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Fish fish = new Fish();
+        Woman woman = new Woman();
+        cat.owner = dog.owner = fish.owner = woman;
+    }
+
+    public static class Cat {
+        public Woman owner;
+    }
+
+    public static class Dog {
+        public Woman owner;
+    }
+
+    public static class Fish {
+        public Woman owner;
+    }
+
+    public static class Woman {
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0216/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task02.task0216;
+
+/* 
+Минимум трёх чисел
+*/
+public class Solution {
+    public static int min(int a, int b, int c) {
+        //напишите тут ваш код
+        int min = a;
+        if (b < min)
+            min = b;
+        if (c < min)
+            min = c;
+        return min;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(min(1, 2, 3));
+        System.out.println(min(-1, -2, -3));
+        System.out.println(min(3, 5, 3));
+        System.out.println(min(5, 5, 10));
+    }
+}
